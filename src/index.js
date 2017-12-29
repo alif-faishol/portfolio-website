45abb46477daa27672c8e0b5706b414c7d2a346e
Alif Faishol <alifaishol@gmail.com>
2017-12-29 08:58:03 +0700

    basic router setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
