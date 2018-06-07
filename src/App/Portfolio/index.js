c3082538a5963f3896e9375bf7fdd0a2ee1ef1ea
Alif Faishol <alifaishol@gmail.com>
2018-06-08 06:24:53 +0700

    Porfolio page, better loading handling

--- src/App/Portfolio/index.js
+++ src/App/Portfolio/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {loadData} from 'redux/modules/portfolio'
+import {loadData, toggleLoading} from 'redux/modules/portfolio'
 import api from 'apiHandler'
 import styled from 'styled-components'
 import PortfolioItem from 'App/Portfolio/PortfolioItem'
@@ -19,56 +19,58 @@ class Portfolio extends React.Component {
     })
       .then(res => {
         this.props.loadData(res)
+        this.props.toggleLoading(false)
       })
       .catch(err => console.log(err))
   }
   componentWillReceiveProps(nextProps) {
     if(this.props.match.params !== nextProps.match.params) {
+      this.props.toggleLoading(true)
       api.getPortfolioItems({
         page:nextProps.match.params.page ? nextProps.match.params.page : 1
       })
         .then(res => {
           nextProps.loadData(res)
+          this.props.toggleLoading(false)
         })
         .catch(err => console.log(err))
     }
   }
   render() {
     return (
-      <div>
-        {(this.props.items.data
-          && this.props.items.data[0])
-            ? (
-              <div>
-                <PortfolioItemsContainer>
-                  {this.props.items.data.map(item => (
-                    <PortfolioItem
-                      key={item.id}
-                      title={item.title}
-                      thumbnail={item.thumbnail}
-                      images={item.images}
-                    />
-                  ))}
-                </PortfolioItemsContainer>
-                <Paginator
-                  totalPage={this.props.items.meta.totalPage}
-                  activePage={this.props.match.params.page || 1}
-                  baseUrl={'/portfolio/'}
+      (this.props.loading
+        ? (<div>Loading</div>)
+        : (
+          <div>
+            <PortfolioItemsContainer>
+              {this.props.items.data.map(item => (
+                <PortfolioItem
+                  key={item.id}
+                  title={item.title}
+                  thumbnail={item.thumbnail}
+                  images={item.images}
                 />
-              </div>
-            )
-            : (<div>Loading</div>)
-        }
-      </div>
+              ))}
+            </PortfolioItemsContainer>
+            <Paginator
+              totalPage={this.props.items.meta.totalPage}
+              activePage={this.props.match.params.page || 1}
+              baseUrl={'/portfolio/'}
+            />
+          </div>
+        )
+      )
     )
   }
 }
 
 export default connect(
   ({main, portfolio}) => ({
-    items: portfolio.items
+    items: portfolio.items,
+    loading: portfolio.loading
   }),
   dispatch => ({
     loadData: data => dispatch(loadData(data)),
+    toggleLoading: state => dispatch(toggleLoading(state))
   })
 )(Portfolio)
--- src/redux/modules/portfolio.js
+++ src/redux/modules/portfolio.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux'
 
 const DATA_LOAD = 'App/Portfolio/DATA_LOAD'
+const LOADING_TOGGLE = 'App/Portfolio/LOADING_TOGGLE'
 
 
 const items = (state={}, action) => (
@@ -9,13 +10,25 @@ const items = (state={}, action) => (
     : state
 )
 
+const loading = (state=true, action) => (
+  action.type === LOADING_TOGGLE
+    ? action.value !== undefined ? action.value : !state
+    : state
+)
+
 export default combineReducers({
   items,
+  loading
 })
 
 
 export const loadData = value => ({
   type: DATA_LOAD,
+  value
+})
+
+export const toggleLoading = value => ({
+  type: LOADING_TOGGLE,
   value
 })
 
